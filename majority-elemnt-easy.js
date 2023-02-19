38e0f0b5cd97f2809cb30cb03d23c868904825e5
rohitgirotra <rohit.girotra@nagarro.com>
2023-02-19 19:50:39 +0530

    final commit

---
+++ majority-elemnt-easy.js
@@ -0,0 +1,20 @@
+var majorityElement = function (nums) {
+     let countOfCurrentVariable = 0;
+     let countOfPreviousVariable = 0;
+     let majorElement = null;
+     nums.sort((a, b) => a - b);
+     for (let i = 0; i < nums.length; i++) {
+          if (nums.length === 1) {
+               majorElement = nums[i];
+               break;
+          } else if (nums[i] === nums[i + 1]) countOfCurrentVariable++;
+          else {
+               if (countOfCurrentVariable > countOfPreviousVariable) {
+                    countOfPreviousVariable = countOfCurrentVariable;
+                    countOfCurrentVariable = 0;
+                    majorElement = nums[i];
+               }
+          }
+     }
+     return majorElement;
+};
